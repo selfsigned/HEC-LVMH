3ea84ebf38b8958f71ab3610b2cd74f791c62d8a
Selfsigned <me@selfsigned.dev>
2024-01-24 03:13:07 +0100

    Add booking flow and bump vite

---
+++ src/lib/booking/bookingscreen.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Icon from '@iconify/svelte';
+
+	const hideClass = 'opacity-60';
+
+	export let serviceData;
+	export let date;
+	export let dayMode = false;
+	export let hourMode = false;
+	export let confirmMode = false;
+</script>
+
+<div class="flex w-full flex-col items-center align-middle">
+	<h1 class="mb-6 text-xl sm:text-2xl">
+		Your {serviceData.name} apointment
+	</h1>
+	<div class="text-md breadcrumbs mb-8 rounded-full px-6 shadow-xl outline outline-2">
+		<ul>
+			<li class={dayMode ? 'underline' : hideClass}>
+				<Icon icon="mdi:calendar"></Icon>{date.toLocaleString('en-us', {
+					weekday: 'short'
+				})}.{date.getDate()}
+			</li>
+			<li class={hourMode ? 'underline' : hideClass}><Icon icon="mdi:clock"></Icon>Hour</li>
+			<li class={confirmMode ? 'underline' : hideClass}>
+				<Icon icon="mdi:check-bold"></Icon>Finish
+			</li>
+		</ul>
+	</div>
+	<slot />
+</div>
--- src/lib/productinfo.svelte
+++ src/lib/productinfo.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { itemData } from '$lib/appstore.js';
+	import Booking from '$lib/booking/booking.svelte';
 
 	export let id; // ID of the product to display infos of
 
@@ -10,7 +11,7 @@
 	{#if $itemData[id].categories.includes('product')}
 		<iframe class="h-full w-full" title="Sephora website" src={sephoraProductUrl + id + '.html'} />
 	{:else}
-		Imagine a super cool appointment booking page here please
+		<Booking {id} />
 	{/if}
 {:else}
 	<p>Error: {id} does not exist</p>

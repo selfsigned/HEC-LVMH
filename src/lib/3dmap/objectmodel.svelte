1033ba4db6ea5d42cac65c85ed92400e40e3c7b8
Nils <80390054+nils-mathieu@users.noreply.github.com>
2024-01-24 12:39:28 +0100

    Adding 3d models. (#18)
    * rename shelf into object
    
    * add 3d model loading support
    
    * move camera
    
    * remove rotation on cards
    
    * replace all shelves with 3d models
    
    * move 3d models
    
    * place text properly

---
+++ src/lib/3dmap/objectmodel.svelte
@@ -0,0 +1,74 @@
+<script context="module">
+	function randomSalt() {
+		return Math.random().toString(36).substring(2, 15);
+	}
+</script>
+
+<script>
+	import { objectsData, itemData, currentItem } from '$lib/appstore.js';
+	import ProductCard from '$lib/productcard.svelte';
+	import { T } from '@threlte/core';
+	import { GLTF, HTML, Text } from '@threlte/extras';
+	import { base } from '$app/paths';
+
+	export let id;
+	export let cardRotation = 0;
+
+	$: {
+		if (!$objectsData[id]) {
+			throw new Error('Object ' + id + ' does not exist');
+		}
+	}
+
+	$: object = $objectsData[id];
+	$: currentItemData = $itemData[$currentItem];
+
+	$: rotation = object.rot ? (object.rot / 180) * Math.PI : 0;
+	$: scale = object.scale || 1;
+	$: textOffset = object.textOffset || [0, 0, 0];
+	$: cardOffset = object.cardOffset || [0, 0, 0];
+</script>
+
+<T.Mesh
+	position={object.pos}
+	rotation={[0, rotation, 0]}
+	scale={[scale, scale, scale]}
+	castShadow
+	receiveShadow
+>
+	{#if currentItemData && currentItemData.object == id}
+		<HTML
+			occlude
+			position={cardOffset}
+			rotation={[0, -rotation + cardRotation, 0]}
+			scale={[0.5 / scale, 0.5 / scale, 0.5 / scale]}
+			transform
+		>
+			<ProductCard enableInfoBtn={true} id={$currentItem} />
+		</HTML>
+	{/if}
+	{#if object.name}
+		<Text
+			position={textOffset}
+			rotation={[-1.57, 0, 0]}
+			text={object.name}
+			color="black"
+			fontSize={0.25 / scale}
+		></Text>
+	{/if}
+
+	<!-- Display each part of the model. -->
+	{#if object.model}
+		<!-- The randomSalt is used to prevent the GLTF renderer from caching models. This causes
+	     them to not render (or rather, to be moved wherever the model is loaded last.
+		 It's pretty weird). -->
+		<GLTF
+			castShadow
+			receiveShadow
+			url={base + '/models/' + object.model + '?salt=' + randomSalt()}
+		/>
+	{:else}
+		<T.BoxGeometry args={[1, 2, 2]}></T.BoxGeometry>
+		<T.MeshStandardMaterial color="white" />
+	{/if}
+</T.Mesh>
--- src/lib/3dmap/scene.svelte
+++ src/lib/3dmap/scene.svelte
@@ -1,17 +1,16 @@
 <script>
-	import { Text, HTML } from '@threlte/extras';
+	import { Text } from '@threlte/extras';
 	import { T } from '@threlte/core';
 	import CameraControls from './cameracontrols.svelte';
 	import { itemData, objectsData, currentItem } from '$lib/appstore.js';
-
-	// App logic
-	import ProductCard from '$lib/productcard.svelte';
+	import ObjectModel from './objectmodel.svelte';
 
 	let objects = $objectsData;
 	let cameraControls;
 
 	function resetCameraPosition() {
 		cameraControls.moveTo(0, 0, 0, true);
+		cameraControls.rotateAzimuthTo(0, true);
 	}
 
 	function rotateToItem(itemId) {
@@ -22,17 +21,16 @@
 			resetCameraPosition();
 			return;
 		}
+
 		let object = objects[item.object];
 		if (!object) return;
-		let rot = object.rot || [0, 0, 0];
 
-		cameraControls.rotateAzimuthTo(rot[1], true);
+		cameraControls.rotateAzimuthTo(0.0, true);
 		cameraControls.moveTo(object.pos[0], object.pos[1], object.pos[2], true);
 		cameraControls.rotatePolarTo(1.0, true);
 	}
 
 	$: rotateToItem($currentItem);
-	$: currentItemData = $currentItem in $itemData ? $itemData[$currentItem] : null;
 </script>
 
 <T.PerspectiveCamera makeDefault position={[0, 12, 12]} lookAt.y={0.5}>
@@ -41,33 +39,16 @@
 			cameraControls = ref;
 		}}
 		maxPolarAngle={Math.PI / 2 - 0.1}
+		draggingSmoothTime={0.2}
 	/>
 </T.PerspectiveCamera>
 
-<T.DirectionalLight position={[0, 0, 10]} castShadow />
-<T.AmbientLight intensity={10} />
+<T.DirectionalLight position={[10, 10, 10]} castShadow />
+<T.AmbientLight intensity={2} />
 
 <!-- Display the objects that have been loaded for this scene. -->
 {#each Object.keys(objects) as objectKey (objectKey)}
-	{@const object = objects[objectKey]}
-	<T.Mesh position={object.pos} rotation={object.rot ? object.rot : [0, 0, 0]} castShadow>
-		{#if currentItemData && currentItemData.object == objectKey}
-			<HTML zIndexRange={[0, 100]} position.y={2.5} scale={0.5} transform>
-				<ProductCard enableInfoBtn={true} id={$currentItem}></ProductCard>
-			</HTML>
-		{/if}
-		{#if object.name}
-			<Text
-				position={[0, 1.1, 0.8]}
-				rotation={[-1.57, 0, 1.57]}
-				text={object.name}
-				color="black"
-				fontSize={0.25}
-			></Text>
-		{/if}
-		<T.BoxGeometry args={[1, 2, 2]}></T.BoxGeometry>
-		<T.MeshStandardMaterial color="grey" />
-	</T.Mesh>
+	<ObjectModel id={objectKey} />
 {/each}
 
 <T.Mesh position.z={-5} position.y={3}>
@@ -79,7 +60,7 @@
 </T.Mesh>
 
 <!-- Floor -->
-<T.Mesh position.y={0} receiveShadow>
-	<T.BoxGeometry args={[10, 0, 10]}></T.BoxGeometry>
+<T.Mesh position.y={-0.1} receiveShadow>
+	<T.BoxGeometry args={[10, 0.1, 10]}></T.BoxGeometry>
 	<T.MeshStandardMaterial color="white" />
 </T.Mesh>
